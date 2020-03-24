33237faa890a8bdabe97b1d0e3d31b6f5173be68
Charles-Clément Gross <60933720+CharlesMacnab@users.noreply.github.com>
2020-03-24 17:14:52 +0100

    Add files via upload

--- TP6/Exercice2.php
+++ TP6/Exercice2.php
@@ -8,19 +8,23 @@ class formulaire{
 
     public function __construct($fichier,$m )
     {
-        $this->formulaire = "<form action = "."$fichier"." method="."$m></form>";
+        $this->formulaire = "<form action = '".$fichier."' method='".$m."'></form>";
     }
 
     public function ajouterzonetexte($texte){
-    $mytext = $texte." : <input type='text' name'".$texte."'/>";
-    $position = strrpos($this->formulaire,"<",-1);
+    $mytext = $texte." : <input type='text' name='".$texte."'>";
+    $position = strrpos($this->formulaire,'<',-1);
     $part1 = substr($this->formulaire,0,$position);
     $part2 = substr($this->formulaire,$position);
     $this->formulaire = $part1.$mytext.$part2;
     }
 
     public function ajouterbouton($texte){
-       $this->formulaire = "<input type="."submit"." value="."$texte"." />";
+        $mytext = "<input type="."submit"." value=".$texte." />";
+        $position = strrpos($this->formulaire,'<',-1);
+        $part1 = substr($this->formulaire,0,$position);
+        $part2 = substr($this->formulaire,$position);
+        $this->formulaire = $part1.$mytext.$part2;
     }
 
     public function getform(){
--- TP6/testformulaire.php
+++ TP6/testformulaire.php
@@ -5,9 +5,10 @@
 include "Exercice2.php";
 
 $test1 = new formulaire("testformulaire.php","post");
-$test1->ajouterzonetexte("Votre nom : ");
-$test1->ajouterzonetexte("Votre code : ");
+$test1->ajouterzonetexte("Votre nom ");
+$test1->ajouterzonetexte("Votre code ");
 $test1->ajouterbouton("Envoyer");
-$test1->getform();
+echo $test1->getform();
+
 
 ?>
